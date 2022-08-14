55d38ebab2fb55dae3847f4869e851a35b4f526a
“naman-nagwani” <namannagwani@gmail.com>
2022-08-14 20:02:18 +0530

    files added

---
+++ src/com/bridgelabz/factor.java
@@ -0,0 +1,29 @@
+/*
+Factors
+a. Desc -> Computes the prime factorization of N using brute force.
+b. I/P -> Number to find the prime factors
+c. Logic -> Traverse till i*i <= N instead of i <= N for efficiency.
+d. O/P -> Print the prime factors of number N.
+*/
+
+package javaClass.assignment.day5;
+
+import java.util.Scanner;
+
+public class factor {
+    public static void main(String[] args) {
+        int n,temp;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Number to find prime factors of");
+        n = scan.nextInt();
+        temp=n;
+
+        for (int i = 2; i * i <= n; i++) {
+
+            while (temp%i==0){
+                System.out.println(i);
+                temp=temp/i;
+            }
+        }
+    }
+}
---
+++ src/com/bridgelabz/triplets.java
@@ -0,0 +1,42 @@
+/*
+Sum of three Integer adds to ZERO
+a. Desc -> A program with cubic running time. Read in N integers and counts the
+number of triples that sum to exactly 0.
+b. I/P -> N number of integer, and N integer input array
+c. Logic -> Find distinct triples (i, j, k) such that a[i] + a[j] + a[k] = 0
+d. O/P -> One Output is number of distinct triplets as well as the second output is to
+print the distinct triplets.
+*/
+package javaClass.assignment.day5;
+
+import java.util.Scanner;
+
+public class triplets {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n,x=0,y=1,count=0;
+        System.out.println("Enter any Number");
+        n = scan.nextInt();
+        int a[]=new int[n];
+        for (int i=0;i<n;i++){
+            System.out.println("Enter any number");
+            a[i] = scan.nextInt();
+        }
+        while (x!=n&&y!=n) {
+            for (int i = 0; i < n; i++) {
+                if (i == x || i == y ) {
+                    continue;
+                } else if ((a[x] + a[y] + a[i]) == 0) {
+                    count++;
+                    System.out.println(a[x] + "+" + a[y] + "+" + a[i] + " = 0");
+                }
+            }
+            x++;
+            y++;
+        }
+        System.out.println(count);
+
+    }
+
+}
